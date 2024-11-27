bd01d7a4280bd4a7eb93b27241dd23db3513c799
Hardik Rathod <hardikrathod1219@gmail.com>
2024-11-27 19:23:16 +0530

    feat(Logo ticker section): all screens done

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,37 @@
+import acmeLogo from "@/assets/logo-acme.png";
+import apexLogo from "@/assets/logo-apex.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import echoLogo from "@/assets/logo-echo.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import Image from "next/image";
+
+const logos = [
+    { src: acmeLogo, alt: "Acme Logo" },
+    { src: quantumLogo, alt: "Quantum Logo" },
+    { src: echoLogo, alt: "Echo Logo" },
+    { src: celestialLogo, alt: "Celestial Logo" },
+    { src: pulseLogo, alt: "Pulse Logo" },
+    { src: apexLogo, alt: "Apex Logo" },
+];
+
 export const LogoTicker = () => {
-  return null;
+    return (
+        <section className="bg-white py-8 md:py-12">
+            <div className="container">
+                <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+                    <div className="flex flex-none gap-14">
+                        {logos.map(({ src, alt }, index) => (
+                            <Image
+                                src={src}
+                                alt={alt}
+                                key={index}
+                                className="h-8 w-auto"
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
