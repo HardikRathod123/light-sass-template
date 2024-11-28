cb88fee5cfca766913a95bf59edbf8cba841727d
Hardik Rathod <hardikrathod1219@gmail.com>
2024-11-28 17:27:07 +0530

    fix(Call to action section): Image position in tablet and desktop screen

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -6,24 +6,24 @@ import Image from "next/image";
 
 export const CallToAction = () => {
     return (
-        <section className="relative bg-gradient-to-b from-white to-indigo-200 py-24">
-            <Image
-                className="absolute -left-[235px] -top-[50px] hidden w-[360px] md:block"
-                alt="Star image"
-                src={starImage}
-            />
-            <Image
-                className="absolute -right-[215px] top-[70px] w-[360px]"
-                alt="Spring image"
-                src={springImage}
-            />
+        <section className="bg-gradient-to-b from-white to-indigo-200 py-24">
             <div className="container">
-                <div className="section-heading">
+                <div className="section-heading relative">
                     <h2 className="section-title">Sign up for free today</h2>
                     <p className="section-description mt-4">
                         Celebrate the joy of accomplishment with an app designed
                         to track your progress and motivate your efforts.
                     </p>
+                    <Image
+                        className="absolute -left-[350px] -top-[137px] hidden size-[360px] md:block"
+                        alt="Star image"
+                        src={starImage}
+                    />
+                    <Image
+                        className="absolute -right-[331px] -top-[20px] size-[360px]"
+                        alt="Spring image"
+                        src={springImage}
+                    />
                 </div>
                 <div className="mt-10 flex justify-center gap-2">
                     <Button>Get for free</Button>
