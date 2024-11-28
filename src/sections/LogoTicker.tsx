96caf9bcb58d61b43ee83aede97d9b42d582df5e
Hardik Rathod <hardikrathod1219@gmail.com>
2024-11-28 19:09:50 +0530

    feat(Logo ticker section): Animations added

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,10 +1,13 @@
+"use client";
 import acmeLogo from "@/assets/logo-acme.png";
 import apexLogo from "@/assets/logo-apex.png";
 import celestialLogo from "@/assets/logo-celestial.png";
 import echoLogo from "@/assets/logo-echo.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import quantumLogo from "@/assets/logo-quantum.png";
+import { motion } from "framer-motion";
 import Image from "next/image";
+import { Fragment } from "react";
 
 const logos = [
     { src: acmeLogo, alt: "Acme Logo" },
@@ -20,16 +23,28 @@ export const LogoTicker = () => {
         <section className="bg-white py-8 md:py-12">
             <div className="container">
                 <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-                    <div className="flex flex-none gap-14">
-                        {logos.map(({ src, alt }, index) => (
-                            <Image
-                                src={src}
-                                alt={alt}
-                                key={index}
-                                className="h-8 w-auto"
-                            />
+                    <motion.div
+                        className="flex flex-none gap-14 pr-14"
+                        animate={{ translateX: "-50%" }}
+                        transition={{
+                            duration: 20,
+                            repeat: Infinity,
+                            ease: "linear",
+                        }}
+                    >
+                        {...new Array(2).fill(null).map((_, index) => (
+                            <Fragment key={index}>
+                                {logos.map(({ src, alt }, index) => (
+                                    <Image
+                                        src={src}
+                                        alt={alt}
+                                        key={index}
+                                        className="h-8 w-auto"
+                                    />
+                                ))}
+                            </Fragment>
                         ))}
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
